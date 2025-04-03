b4501048eb6008d8103a6a952f7dcf42dfa15dc0
deepanm87 <deepan.murugan29@gmail.com>
2025-04-02 22:08:36 -0400

    added event listeners and calculator display functionality

---
+++ script.js
@@ -0,0 +1,19 @@
+const calculatorDisplay = document.querySelector('h1')
+const inputBtns = document.querySelectorAll('button')
+const clearBtn = document.getElementById('clear-btn')
+
+function sendNumber(number) {
+    const displayValue = calculatorDisplay.textContent
+    calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number
+}
+
+inputBtns.forEach( inputBtn => {
+    if(inputBtn.classList.length === 0) {
+        inputBtn.addEventListener('click', () => sendNumber(inputBtn.value))
+    } else if (inputBtn.classList.contains('operator')) {
+        inputBtn.addEventListener('click', () => sendNumber(inputBtn.value))
+    } else if (inputBtn.classList.contains('decimal')) {
+        inputBtn.addEventListener('click', () => sendNumber())
+    }
+})
+
